a44454d93372d9bf7b316904483dc7ca4b8f7218
Muhammad Riaz <muhammadriaz8685@gmail.com>
2025-11-14 18:51:50 +0500

    done some data transformation

---
+++ src/mlproject/components/data_transformation.py
@@ -0,0 +1,19 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import os
+
+
+@dataclass
+class DataTranformstionConfig:
+    preprocessor_obj_file_path  = os.join('artifacts', 'preprocessor.pkl')
+    
