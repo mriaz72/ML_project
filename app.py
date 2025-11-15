96fbb4450ad850fc4087c85687e60883d7986ec2
Muhammad Riaz <muhammadriaz8685@gmail.com>
2025-11-15 16:00:17 +0500

    Update data Ingestion

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
-from src.mlproject.components.data_ingestion import DataIngestion, DataIngestionConfig
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__=="__main__":
